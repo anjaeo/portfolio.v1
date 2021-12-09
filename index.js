78f26e89aa8e6522470eb2f86169e94041106326
anjaeo <anjaeo93@gmail.com>
2021-12-09 17:28:49 -0500

    again

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const app = express()
-const port = process.env.port || 3000;
+const PORT = process.env.PORT || 3000
+const path = require('path')
 
-app.use(express.static('src'))
+app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, 'src', 'css')));
+app.use(express.static(path.join(__dirname, 'src', 'js')));
+app.use(express.static(path.join(__dirname, 'src', 'assets')));
 
 app.get('/', (req, res) => {
   res.render('index')
 })
 
-app.listen(port, () => {
-  console.log(`Portfolio app listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Portfolio app listening at http://localhost:${PORT}`)
 })
